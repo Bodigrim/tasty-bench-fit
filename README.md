08d018ebc89a739ccd3505eb4f04ab260a674ed4
Bodigrim <andrew.lelechenko@gmail.com>
2023-04-12 19:02:17 +0100

    Update documentation

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Benchmark a given function for variable input sizes and find out its time comple
 > fit $ mkFitConfig (\x -> Data.List.sort $ take x $ iterate (\n -> n * 6364136223846793005 + 1) (1 :: Int)) (10, 10000)
 5.2990e-8 * x * log x
 ```
+
+Irregular overhead caused by generational GC may skew observed asymptotics a lot. Sometimes you are lucky to measure things like in-place vector sort or fused list operations like `sum [1..x]`, which do not allocate much. But otherwise you are likely to need a mitigation strategy:
+
+* Consider running measurements with `-O0` or in `ghci` prompt. This is how the usage example above was generated. Without optimizations you will allocate a lot and trigger GC regularly, evening out its effect.
+
+* Another option is to set `+RTS -A128K` roughly to the same effect: smaller nursery causes more regular GC. In this case it is advisable to set the lower bound of input sizes to a relatively large value.
+
+* If it does not work, push benchmarking interval higher, so that the main asymptotic term reveals itself more prominently, and decrease `RelStDev`. This unfortunately does affect running times significantly.
--- src/Test/Tasty/Bench/Fit.hs
+++ src/Test/Tasty/Bench/Fit.hs
@@ -73,13 +73,23 @@ mkFitConfig f (low, high) =
 -- >>> fit $ mkFitConfig (\x -> Data.List.sort $ take x $ iterate (\n -> n * 6364136223846793005 + 1) (1 :: Int)) (10, 10000)
 -- 5.2990e-8 * x * log x
 --
--- This function works best when 'fitBench' routine does not put pressure on garbage
--- collections. Otherwise GC scheduling may greatly skew observed time complexity,
--- unless 'fitLow' is large enough to even out generational GC. The latter event
--- happens earlier if 'fitBench' is made to allocate more, which can be achieved by
--- compiling with @-O0@.
+-- Irregular overhead caused by generational GC may skew observed asymptotics
+-- a lot. Sometimes you are lucky to measure things like in-place vector sort
+-- or fused list operations like 'sum' @[1..x]@, which do not allocate much.
+-- But otherwise you are likely to need a mitigation strategy:
 --
--- When getting surprising results, consider decreasing 'fitRelStDev'.
+-- * Consider running measurements with @-O0@ or in @ghci@ prompt. This is how
+--   the usage example above was generated. Without optimizations you will
+--   allocate a lot and trigger GC regularly, evening out its effect.
+--
+-- * Another option is to set @+RTS -A128K@ roughly to the same effect:
+--   smaller nursery causes more regular GC. In this case it is advisable to
+--   set the lower bound of input sizes to a relatively large value.
+--
+-- * If it does not work, push benchmarking interval higher, so that the main
+--   asymptotic term reveals itself more prominently, and decrease
+--   'RelStDev'. This unfortunately does affect running times significantly.
+--
 fit :: FitConfig -> IO Complexity
 fit FitConfig {..} = do
   let samples = genSamples fitLow fitHigh
